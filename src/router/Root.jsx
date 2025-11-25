62b0f7c2f4686ecc4440add8f9dd8f2746049508
alamin22 <alaminhossan4000@gmail.com>
2025-11-25 21:57:44 +0600

    implement the footer section and design footer based on condition

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router';
 
 const Root = () => {
     return (
-        <div>
+        <div className='my-0'>
             <Header></Header>
             <div className=''>
                 <Outlet></Outlet>
